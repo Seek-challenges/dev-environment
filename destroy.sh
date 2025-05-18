2b172fc4b9d62e4e4cdd487a4ee40a82d4e0aa7d
Carlos Arturo Diaz <diazartiagacarlosarturo@gmail.com>
2025-05-18 02:09:41 -0500

    add destroy script to stop services and remove networks

---
+++ destroy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Bajar servicios por proyecto
+docker-compose -p broker -f compose/broker.yml down
+docker-compose -p cache -f compose/storage.yml down
+docker-compose -p backend -f compose/backend.yml down
+
+# Eliminar redes
+docker network rm public private
