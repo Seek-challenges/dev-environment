d6ad0536f00f94a72d5aae838ec11529bde67e07
Carlos Arturo Diaz <diazartiagacarlosarturo@gmail.com>
2025-05-18 01:49:28 -0500

    Add Docker Compose configurations for Redis, backend, and Consul services

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Crear red pública (bridge)
+docker network create \
+  --driver bridge \
+  --opt com.docker.network.bridge.name=public \
+  public
+
+# Crear red privada (bridge)
+docker network create \
+  --driver bridge \
+  --opt com.docker.network.bridge.name=private \
+  private
+
+# Deploy discovery
+docker-compose -p discovery -f compose/discovery.yml up -d
+
+# Deploy databases
+docker-compose -p storage -f compose/cache.yml up -d
+
+sleep 3
+
+# Deploy backend
+docker-compose -p backend -f compose/backend.yml up -d --scale core-customer=2
