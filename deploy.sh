4f34a4b29e5ed3892e1a99dbbab114a2b6edce59
Carlos Arturo Diaz <diazartiagacarlosarturo@gmail.com>
2025-05-19 17:50:02 -0500

    Refactor Docker configuration for backend services and update deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 119944160132.dkr.ecr.us-east-2.amazonaws.com
-
-
 # Crear red pública (bridge)
 docker network create \
   --driver bridge \
@@ -15,13 +12,14 @@ docker network create \
   --opt com.docker.network.bridge.name=private \
   private
 
-# Deploy discovery
-docker compose -p discovery -f compose/discovery.yml up -d
 
 # Deploy databases
-docker compose -p storage -f compose/cache.yml up -d
+docker-compose -p storage -f compose/storage.yml up -d
+
+# Deploy broker
+docker-compose -p broker -f compose/broker.yml up -d
 
 sleep 3
 
 # Deploy backend
-docker compose -p backend -f compose/backend.yml up -d --scale core-customer=2
+docker-compose -p backend -f compose/backend.yml up -d --scale core-customer=2
--- destroy.sh
+++ destroy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Bajar servicios por proyecto
-docker compose -p discovery -f compose/broker.yml down
-docker compose -p cache -f compose/storage.yml down
-docker compose -p backend -f compose/backend.yml down
+docker-compose -p broker -f compose/broker.yml down
+docker-compose -p storage -f compose/storage.yml down
+docker-compose -p backend -f compose/backend.yml down
 
 # Eliminar redes
 docker network rm public private
